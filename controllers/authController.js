284d860eb491124731b9f347946839d095f46a31
Matt-tedesco98 <116476709+Matt-tedesco98@users.noreply.github.com>
2025-07-12 16:12:42 -0400

    user registration logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,33 @@
 const userModel = require('../models/userModel');
+const bcript = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
-    // TODO: implement user registration logic
+    const { email, password, firstname, lastname } = req.body;
+    if (!email || !password || !firstname || !lastname) {
+        res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    try{
+        const existingUser = await userModel.findUserByEmail(email);
+        if (existingUser) {
+            return res.status(409).json({ error: 'User already exists' });
+        }
+        const hashedPassword = await bcript.hash(password, 10);
+        const newUser = await userModel.createUser({ email, hashedPassword, firstname, lastname });
+        res.status(201).json({
+            message: 'User created successfully',
+            user: {
+                id: newUser.id,
+                email: newUser.email,
+                firstname: newUser.firstname,
+                lastname: newUser.lastname,
+            }
+        });
+    } catch (err) {
+        console.error('register error', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 };
 
 const loginUser = async (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,14 @@
 const db = require('../db');
 
-const createUser = async (userData) => {
-    // TODO: insert user into DB
+const createUser = async ({ email, hashedPassword, firstname, lastname }) => {
+    const result = await db.query('INSERT INTO users (email, password, firstname, lastname) VALUES ($1, $2, $3, $4) RETURNING *', [email, hashedPassword, firstname, lastname]);
+    return result.rows[0];
+
 };
 
 const findUserByEmail = async (email) => {
-    // TODO: fetch user by email
+    const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    return result.rows[0];
 }
 
 module.exports = {
