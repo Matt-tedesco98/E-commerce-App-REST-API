79b123279441a5b9602ab2f0e62523e1330771cf
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-07-14 16:53:52 -0400

    Basic scaffold of Product Controller

---
+++ controllers/productController.js
@@ -0,0 +1,35 @@
+const productModel = require('../models/product');
+
+const getAllProducts = async (req, res) => {
+    try {
+        const result = await productModel.getAllProducts();
+        res.json(result);
+    } catch (err) {
+        console.error('Error getting all products:', err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+};
+
+const getProductById = async (req, res) => {
+    //TODO
+};
+
+const createProduct = async (req, res) => {
+    //TODO
+};
+
+const UpdateProduct = async (req, res) => {
+    //TODO
+};
+
+const deleteProduct = async (req, res) => {
+    //TODO
+}
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+    createProduct,
+    UpdateProduct,
+    deleteProduct,
+}
