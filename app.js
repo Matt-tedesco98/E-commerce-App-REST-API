10b398c881afaf7ed026165d912fa57c54c9fc6b
Matt-tedesco98 <116476709+Matt-tedesco98@users.noreply.github.com>
2025-07-15 12:13:10 -0400

    Set up product endpoints

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
