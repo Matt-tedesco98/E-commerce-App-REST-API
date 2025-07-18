cd4335bd260ea8d7d001628ed504845ea216bb39
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-07-18 14:42:05 -0400

    started cart model

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,2 +1,54 @@
 const db = require('../db/index');
 
+const getCartByUserId = async (userId) => {
+    const result = await db.query(`SELECT *
+                                   FROM cartitems
+                                   where cartid = $1`, [userId]);
+    return result.rows;
+};
+
+const createCart = async (userId) => {
+    const cart = await db.query(`Insert into carts (cartid, created, modified)
+                                 values ($1, now(), now())
+                                 returning *`, [userId]);
+    return cart.rows[0];
+}
+
+const addItemToCart = async (userId, productId, quantity = 1) => {
+    const cartRes = await db.query(`SELECT cartid
+                                    from carts
+                                    where cartid = $1`, [userId]);
+    let cartId = cartRes.rows[0]?.cartid;
+
+    if (!cartId) {
+        const newCart = await createCart(userId);
+        cartId = newCart.cartid;
+    }
+
+    const existingItem = await db.query(`SELECT *
+                                         FROM cartitems
+                                         WHERE cartid = $1
+                                           and productid = $2`, [cartId, productId]);
+
+    if (existingItem.rows.length > 0) {
+        const updatedItem = await db.query(`UPDATE cartitems
+                                          SET quantity = quantity + $1,
+                                              modified = now()
+                                          WHERE cartid = $2
+                                            and productid = $3
+                                          RETURNING *`, [quantity, cartId, productId]);
+        return updatedItem.rows[0];
+    } else {
+        const newItem = await db.query(`INSERT INTO cartitems (cartid, productid, quantity, created, modified)
+                                        VALUES ($1, $2, $3, now(), now())
+                                        returning *`, [cartId, productId, quantity]);
+        return newItem.rows[0];
+    }
+};
+
+
+module.exports = {
+    getCartByUserId,
+    createCart,
+    addItemToCart,
+}
