2860e337c350030ad4952919d25233f7c2e6af04
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-07-19 14:20:36 -0400

    comnpleted order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,2 +1,20 @@
 const db = require('../db/index');
 
+const createOrder = async (userId, total) => {
+    return await db.query(`INSERT INTO orders (userid, total, created, modified)
+                           values ($1, $2, now(), now())
+                           returning *`, [userId, total]);
+};
+
+const addOrderItem = async (orderId, productId, quantity, price) => {
+    const order = await db.query(`INSERT INTO orderitems (orderId, productid, quantity, price, modified, created)
+                                  values ($1, $2, $3, $4, now(), now())
+                                  returning *`, [orderId, productId, quantity, price]);
+    return order.rows[0];
+};
+
+
+module.exports = {
+    createOrder,
+    addOrderItem,
+}
